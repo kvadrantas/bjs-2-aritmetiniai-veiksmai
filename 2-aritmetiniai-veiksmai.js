bede4e17fa705bfdab7d1bb146a21f30acafe13a
Rolandas Seputis <vi.kvadrantas@gmail.com>
2021-08-15 20:20:53 +0300

    Aritmetiniai veiksmai

---
+++ 2-aritmetiniai-veiksmai.js
@@ -0,0 +1,18 @@
+
+
+// %    -   grazina liekana
+// let rez = 100 % 3;
+// console.log(rez);
+// console.log(17 % 3);
+
+/* ARITMETINIAI VEKSMAI
++
+-
+*
+/
+%   -   LIEKANA PO DALYBOS
+**  -   kelimas laipsniu
+
+2**2**3 -   is desines i kaire: pirma 2 pakelia treciuoju ir paskui 2 pakeliam 8-tuoju
+
+*/
